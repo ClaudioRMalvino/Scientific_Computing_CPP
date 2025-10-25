b86d06081c579b83094496d1dc40897024ca9350
Claudio <claudiormal@gmail.com>
2025-10-25 16:10:36 +0100

    worked on runge kutta method. just need to properly plot the two methods.

--- Practical_3/odeSolver.cpp
+++ Practical_3/odeSolver.cpp
@@ -7,14 +7,15 @@ double userMaxT();
 double userDeltaT();
 double userInitY();
 void eulerMethod(const double &maxT, const double &deltaT, const double &initY);
-
+void rungeKutta2ndOrder(const double &maxT, const double &deltaT,
+                        const double &initY);
 int main(void) {
 
   double maxT{userMaxT()};
   double deltaT{userDeltaT()};
   double initY{userInitY()};
   eulerMethod(maxT, deltaT, initY);
-
+  rungeKutta2ndOrder(maxT, deltaT, initY);
   return 0;
 }
 
@@ -38,6 +39,7 @@ double userDeltaT() {
   std::cout << std::endl;
   return deltaT;
 }
+
 double userInitY() {
   std::cout << "Input initial position: ";
   double initY;
@@ -49,20 +51,38 @@ double userInitY() {
 void eulerMethod(const double &maxT, const double &deltaT,
                  const double &initY) {
 
-  std::ofstream dataFile("ode.dat");
+  std::ofstream eulerFile("euler.dat");
 
-  double y0 = initY;
-  double yNext;
+  double y0{initY};
+  double yNext{0.0};
   double time{0.0};
-
-  // dataFile << 0.0 << ", " << y0 << '\n';
 
   while (time < maxT) {
     yNext = y0 + (deltaT * std::sqrt(y0));
 
-    dataFile << time << ", " << yNext << '\n';
+    eulerFile << time << ", " << yNext << '\n';
     y0 = yNext;
     time += deltaT;
   }
-  dataFile.close();
+  eulerFile.close();
+}
+
+void rungeKutta2ndOrder(const double &maxT, const double &deltaT,
+                        const double &initY) {
+  std::ofstream rungeKuttaFile("rungeKutta.dat");
+
+  double y0{initY};
+  double yNext{0.0};
+  double time{0.0};
+  double k1, k2;
+
+  while (time < maxT) {
+    k1 = std::sqrt(y0);
+    k2 = std::sqrt(y0 + k1 * deltaT);
+    yNext = y0 + 0.5 * deltaT * (k1 + k2);
+
+    rungeKuttaFile << time << ", " << yNext << '\n';
+    y0 = yNext;
+    time += deltaT;
+  }
 }
--- Practical_3/odeplotter.py
+++ Practical_3/odeplotter.py
@@ -3,13 +3,22 @@ import numpy as np
 
 
 def main() -> None:
-    file = "ode.dat"
-    data = np.loadtxt(file, delimiter=",")
-    x = data[:, 0]
-    y = data[:, 1]
+    euler_file = "euler.dat"
+    euler_data = np.loadtxt(euler_file, delimiter=",")
+    euler_x = euler_data[:, 0]
+    euler_y = euler_data[:, 1]
+    runge_kutta_file = "rungeKutta.dat"
+    runge_kutta_data = np.loadtxt(runge_kutta_file, delimiter=",")
+    runge_kutta_x = runge_kutta_data[:, 0]
+    runge_kutta_y = runge_kutta_data[:, 1]
 
-    plt.plot(x, y)
+    plt.plot(euler_x, euler_y)
     plt.title("Euler Method")
+    plt.xlabel("t")
+    plt.ylabel("f(y,t)")
+
+    plt.plot(runge_kutta_x, runge_kutta_y)
+    plt.title("Runge Kutta Method")
     plt.xlabel("t")
     plt.ylabel("f(y,t)")
     plt.show()
